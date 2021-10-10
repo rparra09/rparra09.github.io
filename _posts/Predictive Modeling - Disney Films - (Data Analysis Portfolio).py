60ffa2a31163f75f6b42a78156bafd2210988eb5
Richelle P <33273914+rparra09@users.noreply.github.com>
2021-10-10 11:58:57 -0700

    Add files via upload

---
+++ _posts/Predictive Modeling - Disney Films - (Data Analysis Portfolio).py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+#import key libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly as py
+import plotly.express as px
+from sklearn.cluster import KMeans
+
+#import data
+film_data = pd.read_csv("DisneyMoviesDataset.csv", encoding='latin-1')  
+film_data.head()
+
+#drop N/As 
+film_data = film_data.dropna(subset=['rotten_tomatoes', 'Directed by', 'metascore'])
+
+#drop variables not of interest
+film_data = film_data.drop(['Written by', 'Produced by', 'Running time', 'Release date'], axis=1)
+
+## Grou By variable of interest
+film_data.groupby('rotten_tomatoes').count()
+
+## View
+film_data.head()
+
+
+
+#create a pairplot 
+
+## sns.pairplot(film_data)
+
